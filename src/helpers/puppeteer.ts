938599067ba8e922eb630d10b5ff2f6fd6f05398
SkyGopnik <artem.petrunin@list.ru>
2022-04-26 19:14:27 +0300

    Update puppeteer.ts

--- src/helpers/puppeteer.ts
+++ src/helpers/puppeteer.ts
@@ -3,7 +3,9 @@ import puppeteer from "puppeteer";
 export default class Puppeteer {
 
   static async getContent(url: string): Promise<string> {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     await page.goto(url);
