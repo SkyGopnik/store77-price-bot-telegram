37443be2bc74281b95f58774ed05a4c88e06245a
SkyGopnik <artem.petrunin@list.ru>
2022-05-19 17:58:44 +0300

    wip

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -15,7 +15,18 @@ export default async function list(msg: Message) {
     chatId,
     `*Информация о всех отслеживаемых ссылках*\n\n${text.join("\n\n")}`,
     {
-      parse_mode: 'Markdown'
+      parse_mode: 'Markdown',
+      disable_web_page_preview: true,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "/info 1",
+              callback_data: "dwadaw"
+            }
+          ]
+        ]
+      }
     }
   );
 }
